66ea0b8821aa789384bc3d423940b45ae806fb0c
MartinAy <4411119+Martinay@users.noreply.github.com>
2018-09-18 19:27:36 +0200

    added GetFieldIdentifier to status

--- TickTackToe/Game/Status.cs
+++ TickTackToe/Game/Status.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace TickTackToe.Game
 {
@@ -14,5 +16,16 @@ namespace TickTackToe.Game
         public GameStatus GameStatus { get; }
         public Player Player { get; }
         public List<List<Player>> Field { get; }
+
+        private int GetFieldIdentifier()
+        {
+            //Copyright R2D
+            var sb = new StringBuilder();
+            foreach (var outer in Field)
+            foreach (var inner in outer)
+                sb.Append((int)inner);
+
+            return sb.ToString().GetHashCode();
+        }
     }
 }
